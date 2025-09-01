78cb16ee01592c634c353a9a97a95219dd79d0db
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-01 15:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
 
-const team["John", "Bob", "Alice", "Poppy"] = players
+const team = players
 
-const team1 = ["John", "Bob", "Alice", "Poppy"];
+const team1 = [...players];
 
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-const cap1{name, age} = person;
+const cap1 = {...person};
 
-// const p = document.createElement("p");
-// const data= document.getElementById("text")
-// data.appendChild(p)
-// console.log(players) = p.textContent 
 
 window.players = players;
 window.person = person;
